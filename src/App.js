8a4b44633153ba0d752e174252a1385c8a407039
Elzevir De Sousa Sá Filho <safilhoelzevirsafilho@gmail.com>
2021-12-19 20:04:27 -0300

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
+
+  const [state, setstate] = useState(false);
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" id="header">
+        <aside id="button">
+          <button onClick={changeWindow}>
+            <h3>
+              Você confia em mim?
+            </h3>
+          </button>
+        </aside>
+
+        {
+          test()
+        }
       </header>
     </div>
   );
+
+  function changeWindow(event) {
+    document.getElementById("button").style.display = "none";
+    setstate(true);
+  }
+
+  function test() {
+    if(state == true){
+      return (
+        <main>
+          <iframe id="iframe" width={window.innerWidth} height={window.innerHeight} src="https://www.youtube.com/embed/BzV4BkxtyYs?autoplay=1&mute=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+        </main>
+      )
+    }
+  }
 }
 
 export default App;
